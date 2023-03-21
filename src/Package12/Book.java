58e8daeb2e65ff34d3a642c6a89163d4ea2a7220
Antonov333 <antonovsergei333@yahoo.com>
2023-03-21 16:34:38 +0300

    Created Author and Book classes

--- src/Package12/Book.java
+++ src/Package12/Book.java
@@ -3,4 +3,41 @@ package Package12;
 public class Book {
     String name ;
     Author author ;
+    int yearOfPublishing ;
+
+    public Book (String bookName, Author writer, int year) {
+        this.name = bookName;
+        this.author = writer;
+        this.yearOfPublishing = year;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setYearOfPublishing(int yearOfPublishing) {
+        this.yearOfPublishing = yearOfPublishing;
+    }
+
+    public void setAuthor(Author authorOfBook) {
+        this.author = authorOfBook ;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYearOfPublishing() {
+        return yearOfPublishing;
+    }
+    public void printBookInfo() {
+        System.out.println("Автор: " + this.author.getFirstName() + this.author.getLastname());
+        System.out.println("Название: " + this.getName());
+        System.out.println("Год издания: " + this.getYearOfPublishing());
+    }
+
 }
--- src/Package12/Homework12.java
+++ src/Package12/Homework12.java
@@ -1,13 +1,36 @@
 package Package12;
 
+import java.util.Arrays;
+
 public class Homework12 {
 
     public static int homeworkNumber = 12;
     public static void main(String[] args) {
         printTitle(homeworkNumber);
 
-        Author Beaumarchais = new Author("Пьер", "Бомарше") ;
-        System.out.println(Beaumarchais.getFirstName()+" "+Beaumarchais.getLastname());
+        Author beaumarchais = new Author("Пьер", "Бомарше") ;
+       // System.out.println(Beaumarchais.getFirstName()+" "+Beaumarchais.getLastname());
+
+        Book marriageOfFigaro = new Book("Figaro", beaumarchais,2015) ;
+        marriageOfFigaro.setAuthor(beaumarchais);
+        marriageOfFigaro.setYearOfPublishing(2015);
+        marriageOfFigaro.setName("Безумный день, или Женитьба Фигаро (сборник)");
+        Author writer = marriageOfFigaro.getAuthor();
+        System.out.println("Автор книги: " + writer.getFirstName()+ " " + writer.getLastname());
+        System.out.println("Название: " + marriageOfFigaro.getName());
+        System.out.println("Год издания: " + marriageOfFigaro.getYearOfPublishing() + "\n");
+
+        Author baltiAndGalloway = new Author("Haythem Balti,", "Alan Galloway") ;
+
+        Book jobReadyJava = new Book("Job Ready Java", baltiAndGalloway , 0);
+
+        jobReadyJava.printBookInfo();
+
+        jobReadyJava.setYearOfPublishing(2021);
+
+        System.out.println("");
+        jobReadyJava.printBookInfo();
+
 
 
         printBottomMessage(homeworkNumber, "в работе");
