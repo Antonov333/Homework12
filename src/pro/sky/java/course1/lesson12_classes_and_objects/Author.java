e643d66eb7c413e5b0f13379fd680d1841344e86
Antonov333 <antonovsergei333@yahoo.com>
2023-03-22 17:10:36 +0300

    ... and reformatted

--- src/pro/sky/java/course1/lesson12_classes_and_objects/Author.java
+++ src/pro/sky/java/course1/lesson12_classes_and_objects/Author.java
@@ -1,11 +1,12 @@
 package pro.sky.java.course1.lesson12_classes_and_objects;
 
 public class Author {
-    private String firstName;
-    private String lastname ;
+    private final String firstName;
+    private final String lastname;
+
     public Author(String first, String last) {
-        firstName = first ;
-        lastname = last ;
+        firstName = first;
+        lastname = last;
     }
 
     public String getFirstName() {
