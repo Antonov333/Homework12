4ee0448dd92cf88c116ee37bc9233051231475f1
Antonov333 <antonovsergei333@yahoo.com>
2023-03-23 21:53:31 +0300

    Just started with Homework13

---
+++ src/pro/sky/java/course1/Lesson13_Methods_of_objects/Homework13.java
@@ -0,0 +1,7 @@
+package pro.sky.java.course1.Lesson13_Methods_of_objects;
+
+public class Homework13 {
+    public static void main(String[] args) {
+        System.out.println("Just started with homework 13 with Methods of objects");
+    }
+}
